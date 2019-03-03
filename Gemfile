c7d90ff2857521e8fea0f2b4b050d41818446cc2
Ben Drozdoff <bendrozdoff@gmail.com>
2019-03-03 18:17:14 -0500

    basic user authentication backend

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+gem 'devise'
 gem 'haml'
 
 # Use ActiveStorage variant
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_303_035_439) do
+ActiveRecord::Schema.define(version: 20_190_303_214_245) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -32,5 +32,19 @@ ActiveRecord::Schema.define(version: 20_190_303_035_439) do
     t.integer 'starting_price', default: 1, null: false
     t.integer 'final_price'
     t.datetime 'closes_at'
+  end
+
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'first_name', null: false
+    t.string 'last_name', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 end

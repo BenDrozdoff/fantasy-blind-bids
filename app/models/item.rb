f078256786c6d0db8b3332c405aa91097b62f96a
Ben Drozdoff <bendrozdoff@gmail.com>
2019-03-17 15:26:46 -0400

    generate bid model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@
 
 class Item < ApplicationRecord
   belongs_to :auction
+  has_many :bids, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_and_belongs_to_many :auctions, join_table: :memberships
+  has_many :bids, dependent: :destroy
 
   def full_name
     [first_name, last_name].join(' ')

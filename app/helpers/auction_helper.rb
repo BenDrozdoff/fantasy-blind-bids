fbf65b05a6c0c6e8b48c1e8ac34c8888aae784ef
Ben Drozdoff <bendrozdoff@gmail.com>
2019-11-03 23:06:53 -0500

    remove legacy cod

--- app/helpers/auction_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module AuctionHelper
-  def current_user_is_member?(auction)
-    return false unless current_user
-
-    auction.user_ids.include? current_user.id
-  end
-end

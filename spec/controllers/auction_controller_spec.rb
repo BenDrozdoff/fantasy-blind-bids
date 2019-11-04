57a0128c4689e85a4846df6d2c0c73d2979c9bfe
Ben Drozdoff <bendrozdoff@gmail.com>
2019-11-03 23:06:53 -0500

    remove legacy cod

--- spec/controllers/auction_controller_spec.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require "rails_helper"
-
-RSpec.describe AuctionController, type: :controller do
-end
--- spec/controllers/bid_controller_spec.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require "rails_helper"
-
-RSpec.describe BidController, type: :controller do
-end

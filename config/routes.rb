470c329f3213bc27e7fe8866f9d999a24cbe2c95
Ben Drozdoff <bendrozdoff@gmail.com>
2023-02-07 12:47:31 -0500

    Add sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
+require 'sidekiq/web'
 
 Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: redirect("/admin")
+
+  authenticate :user do
+    mount Sidekiq::Web => '/admin/sidekiq'
+  end
 end
